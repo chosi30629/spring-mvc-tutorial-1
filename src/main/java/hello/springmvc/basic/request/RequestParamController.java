7f63150fc7b67760993cedf6aa68a7383492863a
chosi30629 <chosi30629@gmail.com>
2022-01-27 22:34:00 +0900

    HTTP 요청 파라미터 - @ModelAttribute

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -7,10 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -78,6 +80,28 @@ public class RequestParamController {
 	public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
 		
 		log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+		return "ok";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/model-attribute-v1")
+	public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+		log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+		log.info("helloDate={}", helloData);
+		return "ok";
+	}
+	
+	/*
+	 * @ModelAttribute 생략 가능
+	 * 그런데 @RequestParam 도 생략 가능
+	 * 스프링 규칙
+	 * 	- String, int, Integer 같은 단순 타입 = @RequestParam
+	 *  - 나머지 = @ModelAttribute (argument resolver 로 지정해둔 타입 외)
+	 */
+	@ResponseBody
+	@RequestMapping("/model-attribute-v2")
+	public String modelAttributeV2(HelloData helloData) {
+		log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
 		return "ok";
 	}
 	
